8f4431b0db56a0ec9c56b1ec12d249c5f8bc80eb
Mohammed-Rahin <55369695+Mohammed-Rahin@users.noreply.github.com>
2019-10-09 22:18:01 -0400

    Updated File From MR (10/9/2019; 10:17pm)

---
+++ rahin10-8/TotalIncome.java
@@ -0,0 +1,18 @@
+package com.example.budgetm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import static android.util.Log.d;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.TotalIncome);
+
+    }
+}
