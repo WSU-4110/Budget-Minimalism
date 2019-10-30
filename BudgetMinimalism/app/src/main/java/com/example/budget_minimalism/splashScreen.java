a7576c2334f9032a179b7caa41031e86deb29da8
Nahidul Islam <Nahidulislam@wayne.edu>
2019-10-29 22:32:56 -0400

    Made splash screen

--- BudgetMinimalism/app/src/main/java/com/example/budget_minimalism/splashScreen.java
+++ BudgetMinimalism/app/src/main/java/com/example/budget_minimalism/splashScreen.java
@@ -1,14 +1,58 @@
 package com.example.budget_minimalism;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class splashScreen extends AppCompatActivity {
-
+    private int SLEEP_TIMER=3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        LogoLauncher logoLauncher=new LogoLauncher();
+        logoLauncher.start();
+
+
+
+
+
+    }
+
+
+    private class LogoLauncher extends Thread{
+
+        public void run(){
+
+            try{
+
+                sleep(1000*SLEEP_TIMER);
+
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            Intent intent =new Intent(splashScreen.this, MainActivity.class);
+
+            startActivity(intent);
+            splashScreen.this.finish();
+
+
+
+        }
+
+
+
     }
 }
