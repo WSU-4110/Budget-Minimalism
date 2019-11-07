632b4123c03f23f827c998358938ac4bbe097a7a
Mitchell Fenner <mitchfen@protonmail.com>
2019-11-07 13:04:03 -0500

    Applied abstractFactory method to HomePageActivity

--- BM/app/src/main/java/com/example/bm/HomePageActivity.java
+++ BM/app/src/main/java/com/example/bm/HomePageActivity.java
@@ -11,7 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
-public class HomePageActivity extends AppCompatActivity {
+public class HomePageActivity extends AppCompatActivity implements buttonBuilder{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,72 +20,62 @@ public class HomePageActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Mitchell:
-        // This code connects the xml "Expense" button and the java object "button2"
-        // We really need to change the strings.xml file so its not so confusing but...
-        // then it creates a click listener, so that when we click on it
-        // the button calls the openActivity_expense_inputscreen()
-        Button button2 = (Button) findViewById(R.id.button2); // Main menu sends to expense page
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity_expense_inputscreen();
-            }
-        });
+        // override abstract factory method
+        private void mitchButtonBuilder(){
+                    Button button2 = (Button) findViewById(R.id.button2);
+                    Button incomeButton = (Button) findViewById(R.id.incomeButton);
+                    FloatingActionButton fab = findViewById(R.id.fab);
+                    Button viewButton = (Button) findViewById(R.id.button3);
+        };
 
-        // Mitchell:
-        // This code connects the xml "Income" button and the java object "incomeButton"
-        // then it creates a click listener, so that when we click on it
-        // the button calls the openActivity_expense_inputscreen()
-        Button incomeButton = (Button) findViewById(R.id.incomeButton);
-        incomeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity_income_inputscreen();
-            }
-        });
-
-        // This function sends you to the settings page
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Snackbar.make(view, "WRITE CODE TO SEND U TO SETTINGS", Snackbar.LENGTH_LONG)
-                //       .setAction("Action", null).show();
-                sendYoutoSettingsPage();
-            }
-        });
-
-        Button viewButton = (Button) findViewById(R.id.button3);
-        viewButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendYouToViewRecent();
-            }
-        });
+        // override abstract factory method
+        private void listeners(){
+            viewButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sendYouToViewRecent();
+                }
+            });
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Snackbar.make(view, "WRITE CODE TO SEND U TO SETTINGS", Snackbar.LENGTH_LONG)
+                    //       .setAction("Action", null).show();
+                    sendYoutoSettingsPage();
+                }
+            });
+            incomeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openActivity_income_inputscreen();
+                }
+            });
+            button2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openActivity_expense_inputscreen();
+                }
+            });
+        }
     }
-    // Mitchell wrote this function
-    public void openActivity_expense_inputscreen() {
-        Intent intent = new Intent(this, expense_inputscreen.class);
-        startActivity(intent);
-    }
-
-    // Mitchell
-    public void openActivity_income_inputscreen() {
-        Intent intent = new Intent(this, income_inputscreen.class);
-        startActivity(intent);
-    }
-
-    // Mitchell
-    public void sendYoutoSettingsPage() {
-        Intent intent = new Intent (this, settingsPage.class);
-        startActivity(intent);
-    }
-
-    // This function sends you to the view_recent_budget_activity
-    public void sendYouToViewRecent() {
-        Intent intent = new Intent (this, selectWhichViewPage.class);
-        startActivity(intent);
+    // override abstract factory method
+    public void sender() {
+        public void openActivity_expense_inputscreen() {
+            Intent intent = new Intent(this, expense_inputscreen.class);
+            startActivity(intent);
+        }
+        public void openActivity_income_inputscreen() {
+            Intent intent = new Intent(this, income_inputscreen.class);
+            startActivity(intent);
+        }
+        public void sendYoutoSettingsPage() {
+            Intent intent = new Intent(this, settingsPage.class);
+            startActivity(intent);
+        }
+        public void sendYouToViewRecent() {
+            Intent intent = new Intent(this, selectWhichViewPage.class);
+            startActivity(intent);
+        }
     }
 
     // Android Studio default code
@@ -111,5 +101,4 @@ public class HomePageActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
---
+++ BM/app/src/main/java/com/example/bm/buttonBuilder.java
@@ -0,0 +1,8 @@
+package com.example.bm;
+
+// Mitchell's Abstract factory
+public interface buttonBuilder {
+    private abstract void listener();
+    private abstract void mitchButtonBuilder();
+    private abstract void sender();
+}
