e15c1d86089211785c17a759e3e79f1a56e60e61
Mitchell Fenner <mitchfen@protonmail.com>
2019-11-21 19:19:08 -0500

    View page update

---
+++ BM/app/src/main/java/com/example/bm/userInterfaceJava/view_spending_delta.java
@@ -0,0 +1,101 @@
+package com.example.bm.userInterfaceJava;
+
+import android.app.ProgressDialog;
+import android.os.Handler;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bm.R;
+
+public class view_spending_delta extends AppCompatActivity {
+
+    Button btnStartProgress;
+    ProgressDialog progressBar;
+    private int progressBarStatus = 0;
+    private Handler progressBarHandler = new Handler();
+    private long fileSize = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_spending_delta);
+        addListenerOnButtonClick();
+    }
+
+    public void addListenerOnButtonClick(){
+        btnStartProgress = findViewById(R.id.button);
+        //end of onClick method
+        btnStartProgress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // creating progress bar dialog
+                progressBar = new ProgressDialog(v.getContext());
+                progressBar.setCancelable(true);
+                progressBar.setMessage("Updating ...");
+                progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressBar.setProgress(0);
+                progressBar.setMax(100);
+                progressBar.show();
+                //reset progress bar and filesize status
+                progressBarStatus = 0;
+                fileSize = 0;
+
+                new Thread(new Runnable() {
+                    public void run() {
+                        while (progressBarStatus < 100) {
+                            // performing operation
+                            progressBarStatus = doOperation();
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            // Updating the progress bar
+                            progressBarHandler.post(new Runnable() {
+                                public void run() {
+                                    progressBar.setProgress(progressBarStatus);
+                                }
+                            });
+                        }
+                        // performing operation if file is downloaded,
+                        if (progressBarStatus >= 100) {
+                            // sleeping for 1 second after operation completed
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            // close the progress bar dialog
+                            progressBar.dismiss();
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
+    // checking how much file is downloaded and updating the filesize
+    public int doOperation() {
+        //The range of ProgressDialog starts from 0 to 10000
+        while (fileSize <= 10000) {
+            fileSize++;
+            if (fileSize == 1000) {
+                return 10;
+            } else if (fileSize == 2000) {
+                return 20;
+            } else if (fileSize == 3000) {
+                return 30;
+            } else if (fileSize == 4000) {
+                return 40; // you can add more else if
+            }
+         /* else {
+                return 100;
+            }*/
+        }//end of while
+        return 100;
+    }//end of doOperation
+}
+
