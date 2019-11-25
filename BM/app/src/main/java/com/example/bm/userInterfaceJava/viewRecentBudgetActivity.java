441fe9549532927dddd8c55e4670f0582da3ebbb
Nahidul Islam <Nahidulislam@wayne.edu>
2019-11-25 16:00:09 -0500

    Implemented the view page to see total amount of expenses

--- BM/app/src/main/java/com/example/bm/userInterfaceJava/viewRecentBudgetActivity.java
+++ BM/app/src/main/java/com/example/bm/userInterfaceJava/viewRecentBudgetActivity.java
@@ -4,7 +4,9 @@ package com.example.bm;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -12,8 +14,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nullable;
 
@@ -24,9 +32,48 @@ public class viewRecentBudgetActivity extends AppCompatActivity {
     private dataViewModel dataViewModel;
     private static final String TAG = "ListDataActivity";
 
+    private TextView mTotal;
+    private DatabaseReference mDatabase;
+
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_recent_budget_activity);
+        //finds and initializes the total amount text view place
+
+        mTotal = (TextView)findViewById(R.id.totalAmtid);
+        // creates a referance to the child or the root of the database which
+        // i have saved up as transection because it saves any expense transection
+
+        mDatabase= FirebaseDatabase.getInstance().getReference().child("transection");
+
+        //creates an event listener which finds all the prices and sums them up
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int sum=0;
+                // in a for loop, we go through each one and add the price attribute
+
+                for (DataSnapshot ds:dataSnapshot.getChildren()){
+                    Map<String, Object> map=(Map<String,Object>) ds.getValue();
+                    // looks for price in the database to add
+                    Object price=map.get("price");
+                    // coverting string to int using javas built in method parseint
+
+                    int pValue=Integer.parseInt(String.valueOf(price));
+                    sum+=pValue;
+                    // sums up and sends to the text field in the xml file.
+                    mTotal.setText(String.valueOf(sum));
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         // Mitchell
         // This code sets up the recycler view to return database information
